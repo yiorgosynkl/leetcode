cdc5e1623e4340822d20688456a37337acbe36ac
Yiorgos Yiannakoulias <yiorgosynkl@gmail.com>
2020-05-18 18:01:31 +0300

    567 done, day 18 challenge accepted

--- 30_day_challenge_2020_May/567_permutation_in_string_day18.py
+++ 30_day_challenge_2020_May/567_permutation_in_string_day18.py
@@ -27,3 +27,21 @@ class Solution:
                 return True
             window_hash -= hash(s2[i - len(s1) + 1])            
         return False
+
+    # solution by awice
+    # def checkInclusion(self, s1, s2):
+    #     A = [ord(x) - ord('a') for x in s1]
+    #     B = [ord(x) - ord('a') for x in s2]
+        
+    #     target = [0] * 26
+    #     for x in A:
+    #         target[x] += 1
+        
+    #     window = [0] * 26
+    #     for i, x in enumerate(B):
+    #         window[x] += 1
+    #         if i >= len(A):
+    #             window[B[i - len(A)]] -= 1
+    #         if window == target:
+    #             return True
+    #     return False
