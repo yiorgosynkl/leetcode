8547da9efc1c5c2386010e0c41966d7462f56bca
Yiorgos Yiannakoulias <yiorgosynkl@gmail.com>
2020-06-21 13:23:34 +0300

    174 done, day 21 challenge accepted

---
+++ 30_day_challenge_2020_June/174_dungeon_game_day21.py
@@ -0,0 +1,19 @@
+################################################################
+# Author            : yiorgosynkl (find me in Github: https://github.com/yiorgosynkl)
+# Date created      : 20200621
+# Problem link      : https://leetcode.com/problems/dungeon-game/
+################################################################
+
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        m, n = len(dungeon), len(dungeon[0])
+        dungeon[m-1][n-1] = max(0, -dungeon[m-1][n-1])
+        for j in reversed(range(n-1)):
+            dungeon[m-1][j] = max(0,dungeon[m-1][j+1] - dungeon[m-1][j])
+        for i in reversed(range(m-1)):
+            dungeon[i][n-1] = max(0,dungeon[i+1][n-1] - dungeon[i][n-1])
+        for i in reversed(range(m-1)):
+            for j in reversed(range(n-1)):
+                dungeon[i][j] = min(max(0,dungeon[i][j+1] - dungeon[i][j]), max(0,dungeon[i+1][j] - dungeon[i][j]))
+        print(dungeon)
+        return dungeon[0][0]+1
