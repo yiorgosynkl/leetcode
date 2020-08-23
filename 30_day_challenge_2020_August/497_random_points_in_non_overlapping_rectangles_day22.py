a82b462278a43559f02ef862f1fd43d5cf99471c
Yiorgos Yiannakoulias <yiorgosynkl@gmail.com>
2020-08-23 13:34:00 +0300

    497 done, day 22 challenge accepted

---
+++ 30_day_challenge_2020_August/497_random_points_in_non_overlapping_rectangles_day22.py
@@ -0,0 +1,32 @@
+################################################################
+# Author            : yiorgosynkl (find me in Github: https://github.com/yiorgosynkl)
+# Date created      : 20200822
+# Problem link      : https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/
+################################################################
+
+import bisect
+import random
+
+class Solution:
+
+    def __init__(self, rects: List[List[int]]):
+        self.rects = []
+        self.n_points = 0
+        self.limits = []
+        for x1, y1, x2, y2 in rects:
+            self.rects.append((x1,y1,x2,y2))
+            self.n_points += (x2-x1+1)*(y2-y1+1)
+            self.limits.append(self.n_points)
+
+    def pick(self) -> List[int]:
+        num = random.randint(0, self.n_points-1)
+        idx = bisect.bisect_right(self.limits, num)
+        x1, y1, x2, y2 = self.rects[idx]
+        num, dy = (self.limits[idx] - 1) - num, y2 - y1 + 1
+        x3, y3 = x1 + (num // dy), y1 + (num % dy)
+        return [x3, y3]
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(rects)
+# param_1 = obj.pick()
