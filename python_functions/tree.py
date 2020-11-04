5d5208e72c7f154a4dc4c3bbe314c8445f0d4d0f
Yiorgos Yiannakoulias <yiorgosynkl@gmail.com>
2020-11-04 18:35:38 +0200

    adjacency list for graphs and bfs function added

--- python_functions/tree.py
+++ python_functions/tree.py
@@ -13,3 +13,28 @@ def inorder(root): # dfs
 
 def postorder(root): # dfs
     return dfs(root.left) + dfs(root.right) + [root.val] if root else []
+
+from collections import defaultdict
+
+# create graph from list of edges
+# number of nodes, edges between nodes
+def create_adj(n, edges):
+    adj = defaultdict(set)
+    for i, j in edges:
+        adj[i].add(j)
+        adj[j].add(i)
+
+def bfs(adj, root):
+    prv, lvl = [], [root] # nodes of previous and current level [list]
+    xpl = { root } # exlored nodes {set}
+    ht = -1 # height (int)
+    while lvl and ht < stop_ht:
+        nxt = [] # nodes of next level
+        ht += 1
+        for node in lvl:
+            for nb in adj[node]: # find unexplored neighbors
+                if nb not in xpl:
+                    nxt.append(nb)
+                    xpl.add(nb)
+        prv, lvl = lvl, nxt
+    return ht, prv # return height and nodes of last lvl
